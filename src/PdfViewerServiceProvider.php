6cfc1841eb475a219cb8b6db17927cf25c6afecd
Nelson <katalenelson@gmail.com>
2020-09-12 00:09:00 +0300

    Add: pdf view service provider

--- src/PdfViewerServiceProvider.php
+++ src/PdfViewerServiceProvider.php
@@ -1,1 +1,27 @@
-<?
+<?php
+/* 
+    Author: Nelson K
+*/
+namespace Nelsonkats\Pdfviewer;
+
+use Carbon\Laravel\ServiceProvider;
+
+class PdfViewerServiceProvider extends ServiceProvider{
+
+
+    /* 
+        Bootstrap application services
+    */
+    public function boot()
+    {
+
+    }
+
+    /* 
+        Register application's services
+    */
+    public function register()
+    {
+
+    }
+}
